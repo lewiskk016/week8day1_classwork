e40d42b6348822588f3a7e6238402bf768ea5806
lewiskk016 <lewiskk016@gmail.com>
2023-04-24 16:26:19 -0400

    finish phase I

--- skeleton 4/bin/p01_basic_server.rb
+++ skeleton 4/bin/p01_basic_server.rb
@@ -1,1 +1,16 @@
 require 'rack'
+
+
+
+app1 = Proc.new do |env|
+    req = Rack::Request.new(env)
+    path = req.path
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    res.write(path)
+    res.finish
+end
+
+Rack::Server.start(
+    app: app1,
+    Port: 3000)
